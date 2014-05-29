611a8794e423f30ddb622abf17fa9d80509773f4
mcentis <matteo.centis.vignali@desy.de>
2014-05-29 13:33:55 +0200

    added scripts to launch analysis and modify the config files

---
+++ scripts/launchAnalysis.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#usage . ./launchAnalysis directory maxProc
+#directory is the directory to look for files, with the structure specified below, and maxProc is the max number of processes to execute at the same time (should be equal to te number of cpus)
+#read the root files in a directory structure like base/sensor/bias where the bias folder contains the root files and a .cfg
+#the base directory should be given as argument
+#the root files are expected to have the .root extension
+
+maxjobs=$2
+jobCount=0 #number of daughter jobs
+
+for iSens in $(ls -d $1/*/)
+do
+    #echo $iSens
+    for iBias in $(ls -d $iSens/*/)
+    do
+#echo $iBias
+	for iRun in $(ls -d $iBias/*.root)
+	do
+	    iConf=$(ls -d $iBias/*.cfg)
+	    xterm -e "cd $PWD; pwd; ./executables/chargeDistr $iRun $iConf" &
+	    let jobCount+=1 #increment job coutner
+#echo "======================================================================bla $iRun $iConf"
+	    if [ $jobCount -eq $maxjobs ] #if the number of submitted jobs is equal to the maximum, wait for the last of them to finish
+	    then
+# echo waiting
+# echo $jobCount
+		#wait $! #wait for the last submitted job to finish
+		wait #wait for the submitted jobs to finish
+		jobCount=0
+	    fi
+	done
+    done
+done
+
+wait # wait for all jobs to finish
---
+++ scripts/modifyCfg.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#usage . ./modifyCfg.sh directory find replace
+
+#uses sed to modify the config files in a directory structure like base/sensor/bias
+#the base directory should be given as argument
+#the config files are expected to have the .cfg extension
+
+for iSens in $(ls -d $1/*/)
+do
+    #echo $iSens
+    for iBias in $(ls -d $iSens/*/)
+    do
+#echo $iBias
+	iConf=$(ls -d $iBias/*.cfg)
+	if [ ! -z $iConf ] # exclude the case of files not found
+	then
+#echo "======================================== $iConf"
+	    sed -i "s|$2|$3|g" $iConf
+	fi
+    done
+done
